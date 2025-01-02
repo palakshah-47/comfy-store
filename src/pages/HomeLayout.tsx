19a5c86a64e50ef5c3c2db9a0e91f3741c5ce97d
Palak Shah <palak.shah@solvassolutions.com>
2025-01-02 17:34:31 -0500

    adding navbar components

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,12 +1,13 @@
-import { Header } from "@/components";
+import { Header, Navbar } from "@/components";
 import { Outlet } from "react-router-dom";
 
 function HomeLayout() {
   return (
     <>
       <Header></Header>
-      <nav>navbar</nav>
-      <div className="aling-element py-20">
+
+      <Navbar />
+      <div className="items-center align-middle p-20">
         <Outlet />
       </div>
     </>
