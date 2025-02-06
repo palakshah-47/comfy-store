93a0e9dab24dd644b764c053cd823b409579f8f4
Palak Shah <palak.shah@solvassolutions.com>
2025-02-06 17:26:46 -0500

    adding Loading with skeleton

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,5 +1,18 @@
+import { Skeleton } from "./ui/Skeleton";
+
 const Loading = () => {
-  return <h2 className="text-xl">Loading...</h2>;
+    return <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {Array.from({ length: 3 }).map((_, index) => {
+            return <div key={index} className="flex flex-col space-y-3">
+                <Skeleton className="h-[125px] w-full rounded-xl"></Skeleton>
+                <div className="space-y-2">
+                    <Skeleton className="h-4 mx-auto w-[250px]"></Skeleton>
+                    <Skeleton className="h-4 mx-auto w-[250px]"></Skeleton>
+                </div>
+                
+          </div>
+      })}
+  </div>;
 };
 
 export default Loading;
