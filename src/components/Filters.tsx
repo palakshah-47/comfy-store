0867fb47f071f45fcf7c8add2327ceb260548788
Palak Shah <palak.shah@solvassolutions.com>
2025-02-28 18:30:12 -0500

    adding formInout and formSelect

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,26 +1,45 @@
 import { Form, useLoaderData, Link } from "react-router-dom";
-import { Input } from "@/components/ui/Input";
-import { Label } from "@/components/ui/Label";
 import { Button } from "@/components/ui/button";
 import { ProductResponseWithParams } from "@/utils";
+import FormInput from "./FormInput";
+import FormSelect from "./FormSelect";
 
 function Filters() {
   const { meta, params } = useLoaderData() as ProductResponseWithParams;
-  const { search } = params;
+  const { search, company, category, shipping, order, price } = params;
   return (
     <Form
       className="border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 
     sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center"
     >
-      <div className="mb-2">
-        <Label htmlFor="search">Search Product</Label>
-        <Input
-          id="search"
-          name="search"
-          type="text"
-          defaultValue={search}
-        ></Input>
-      </div>
+      {/* Search */}
+      <FormInput
+        type="search"
+        label="search product"
+        name="search"
+        defaultValue={search}
+      />
+      {/* CATEGORIES */}
+      <FormSelect
+        label="select category"
+        name="category"
+        options={meta.categories}
+        defaultValue={category}
+      />
+      {/* COMPANIES */}
+      <FormSelect
+        label="select company"
+        name="company"
+        options={meta.companies}
+        defaultValue={company}
+      />
+      {/* ORDER */}
+      <FormSelect
+        label="order by"
+        name="order"
+        options={["a-z", "z-a", "high", "low"]}
+        defaultValue={order}
+      />
       <Button type="submit" size="sm" className="self-end mb-2">
         Search
       </Button>
---
+++ src/components/FormSelect.tsx
@@ -0,0 +1,47 @@
+import {
+  Select,
+  SelectContent,
+  SelectTrigger,
+  SelectItem,
+  SelectValue
+} from "@/components/ui/Select";
+
+import { Label } from "@/components/ui/Label";
+
+type SelectInputProps = {
+  name: string;
+  label?: string;
+  defaultValue?: string;
+  options: string[];
+};
+
+const FormSelect = ({
+  label,
+  name,
+  options,
+  defaultValue
+}: SelectInputProps) => {
+  return (
+    <div className="mb-2">
+      <Label htmlFor={name} className="capitalize">
+        {label || name}
+      </Label>
+      <Select defaultValue={defaultValue || options[0]} name={name}>
+        <SelectTrigger id={name}>
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          {options.map((item, index) => {
+            return (
+              <SelectItem key={`${index}-${item}`} value={item}>
+                {item}
+              </SelectItem>
+            );
+          })}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
+
+export default FormSelect;
