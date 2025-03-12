ea5b7c88e8d7136765219862c76368a1fd3350c6
Palak Shah <palak.shah@solvassolutions.com>
2025-03-12 18:09:42 -0400

    adding pagination container

--- src/utils/pagination.ts
+++ src/utils/pagination.ts
@@ -9,18 +9,32 @@ export const constrcutUrl = ({
   search,
   pathname
 }: ComstructUrlParams): string => {
-  return "/products";
+  const searchParams = new URLSearchParams(search);
+  searchParams.set("page", pageNumber.toString());
+  return `${pathname}?${searchParams.toString()}`;
 };
 
 type ConstructPrevOrNextParams = {
-    currentPage: number;
-    pageCount: number;
-    search: string;
-    pathname: string;
-}
+  currentPage: number;
+  pageCount: number;
+  search: string;
+  pathname: string;
+};
 
-export const constructPrevOrNextUrl = ({ currentPage, pageCount, search, pathname }: ConstructPrevOrNextParams): { prevUrl: string;  nextUrl: string} => {
-    const nextUrl = '/products';
-    const prevUrl = '/products'
-    return {prevUrl, nextUrl}
-}
+export const constructPrevOrNextUrl = ({
+  currentPage,
+  pageCount,
+  search,
+  pathname
+}: ConstructPrevOrNextParams): { prevUrl: string; nextUrl: string } => {
+  let prevPage = currentPage - 1;
+  if (prevPage < 1) prevPage = pageCount;
+  const prevUrl = constrcutUrl({ pageNumber: prevPage, search, pathname });
+
+  let nextPage = currentPage + 1;
+  if (nextPage > pageCount) nextPage = 1;
+
+  const nextUrl = constrcutUrl({ pageNumber: nextPage, search, pathname });
+
+  return { prevUrl, nextUrl };
+};
